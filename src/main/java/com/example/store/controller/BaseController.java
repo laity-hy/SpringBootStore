6fd918fd3313fa25aeb86ab29598d2dddf447faa
l aity <l1810638451@gmail.com>
2022-10-08 15:18:09 +0800

    controller层处理所有抛出的异常

--- src/main/java/com/example/store/controller/BaseController.java
+++ src/main/java/com/example/store/controller/BaseController.java
@@ -19,7 +19,7 @@ public class BaseController {
     //请求处理方法，这个方法的返回值就是需要传递给前端的数据
     //自动将异常对象传递给此方法的参数列表上
     //当项目中产生了异常，被统一拦截到此方法中，这个方法的返回值直接给到前端
-    @ExceptionHandler(ServiceException.class) //用于统一处理抛出的异常
+    @ExceptionHandler({ServiceException.class, FileUploadException.class}) //用于统一处理抛出的异常
     public JsonResult<Void> handleException(Throwable e) {
         JsonResult<Void> result = new JsonResult<>(e);
         if (e instanceof UsernameDuplicatedException) {     //4000异常
