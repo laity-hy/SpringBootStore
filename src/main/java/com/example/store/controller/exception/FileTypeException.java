d5a5b80ee797cc96018b0ef34eb71195113c776e
l aity <l1810638451@gmail.com>
2022-10-08 15:05:38 +0800

    上传头像controller层异常

---
+++ src/main/java/com/example/store/controller/exception/FileTypeException.java
@@ -0,0 +1,26 @@
+package com.example.store.controller.exception;
+
+/**
+ * 文件类型异常
+ */
+public class FileTypeException extends FileUploadException{
+    public FileTypeException() {
+        super();
+    }
+
+    public FileTypeException(String message) {
+        super(message);
+    }
+
+    public FileTypeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public FileTypeException(Throwable cause) {
+        super(cause);
+    }
+
+    protected FileTypeException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
