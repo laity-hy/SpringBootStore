6194c52d6731c4d672f68e8f681629f0847543ae
l aity <l1810638451@gmail.com>
2022-10-09 10:16:17 +0800

    上传头像controller config html修改

--- src/main/java/com/example/store/controller/UserController.java
+++ src/main/java/com/example/store/controller/UserController.java
@@ -1,17 +1,41 @@
 package com.example.store.controller;
 
+import com.example.store.controller.exception.*;
 import com.example.store.entity.User;
 import com.example.store.service.IUserService;
 import com.example.store.util.JsonResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpSession;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/users")
 public class UserController extends BaseController {
+    /**
+     * 设置上传文件的最大值
+     */
+    public static final int AVATAR_MAX_SIZE = 10 * 1024 * 1024;
+    /**
+     * 限制上传文件的类型
+     */
+    public static final List<String> AVATAR_TYPE = new ArrayList<>();
+
+    static {
+        AVATAR_TYPE.add("image/jpeg");
+        AVATAR_TYPE.add("image/png");
+        AVATAR_TYPE.add("image/bmp");
+        AVATAR_TYPE.add("image/gif");
+    }
+
     @Autowired
     private IUserService userService;
 
@@ -20,7 +44,6 @@ public class UserController extends BaseController {
         userService.register(user);
         return new JsonResult<>(OK);
     }
-
 
     @RequestMapping("/login")
     public JsonResult<User> login(String username, String password, HttpSession session) {
@@ -53,5 +76,57 @@ public class UserController extends BaseController {
         //uid数据再次封装到user对象中
         userService.changeInfo(getUidFromSession(session), getUsernameFromSession(session), user);
         return new JsonResult<>(OK);
+    }
+
+    /**
+     * MultipartFile接口是SpringMVC提供一个接口，这个接口为我们包装了获取文件类型的数据（任何类型的file都可以）
+     * SpringBoot它整合了SpringMVC，只需要在处理请求的方法参数列表上声明一个参数类型为MultipartFile的参数，然后SpringBoot自动将传递给服务的文件赋值给这个参数
+     *
+     * @param session
+     * @param file
+     * @return
+     * @RequesParam 表示请求的参数，将请求的参数注入请求处理方法的某个参数上，如果名称不一致则开一个使用@RequestParam注解进行标记和映射
+     */
+    @RequestMapping("/change_avatar")
+    public JsonResult<String> changeAvatar(HttpSession session, @RequestParam("file") MultipartFile file) {
+        //判断文件是否为空
+        if (file.isEmpty()) {
+            throw new FileEmptyException("文件为空");
+        }
+        if (file.getSize() > AVATAR_MAX_SIZE) {
+            throw new FileSizeException("文件超出限制");
+        }
+        //判断文件的类型是否是我们规定的后缀类型
+        String contentType = file.getContentType();
+        //如果集合包含某个元素则返回true
+        if (!AVATAR_TYPE.contains(contentType)) {
+            throw new FileTypeException("文件类型不支持");
+        }
+        //上传的文件.../upload/文件.png
+        String parent = session.getServletContext().getRealPath("upload");
+        //File对象指向这个路径，File是否存在
+        File dir = new File(parent);
+        if (!dir.exists()) {//检测目录是否存在
+            dir.mkdirs();//创建当前的目录
+        }
+        //获取到这个文件名称，UUID工具来将生成一个新的字符串作为文件名
+        String originalFilename = file.getOriginalFilename();
+        int index = originalFilename.lastIndexOf(".");
+        String suffix = originalFilename.substring(index);
+        String filename = UUID.randomUUID().toString().toUpperCase() + suffix;
+        File dest = new File(dir, filename);//是一个空文件
+        //参数file中数据写入到这个空文件中
+        try {
+            file.transferTo(dest);//将file文件中的数据写入到dest文件中
+        } catch (FileStateException e) {
+            throw new FileStateException("文件状态异常");
+        } catch (IOException e) {
+            throw new FileUploadIOException("文件读写异常");
+        }
+        //返回头像的路径/upload/test.png
+        String avatar = "/upload/" + filename;
+        userService.changeAvatar(getUidFromSession(session), avatar, getUsernameFromSession(session));
+        //返回用户头像的路径给前端页面，将来用于头像展示使用
+        return new JsonResult<>(OK, avatar);
     }
 }
