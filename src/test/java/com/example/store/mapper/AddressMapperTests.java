433866facfd35d03dd1acdeb562330205ec2f853
l aity <l1810638451@gmail.com>
2022-10-09 23:46:36 +0800

    新增收货地址mapper层

---
+++ src/test/java/com/example/store/mapper/AddressMapperTests.java
@@ -0,0 +1,31 @@
+package com.example.store.mapper;
+
+import com.example.store.entity.Address;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class AddressMapperTests {
+    @Autowired(required = false)
+    private AddressMapper addressMapper;
+
+    @Test
+    public void insert() {
+        Address address = new Address();
+        address.setUid(12);
+        address.setPhone("123456789");
+        address.setName("女朋友");
+        Integer result = addressMapper.insert(address);
+        System.out.println(result);
+    }
+
+    @Test
+    public void countByUid() {
+        Integer result = addressMapper.countByUid(12);
+        System.out.println(result);
+    }
+}
